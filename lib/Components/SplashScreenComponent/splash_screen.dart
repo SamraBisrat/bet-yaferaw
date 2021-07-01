6c5d5c76e945f26c309364280a9b377420e6add0
rediet <redietmebrat@gmail.com>
2021-07-01 19:42:41 +0300

    splash screen, images, home, masterprovider, main

---
+++ lib/Components/SplashScreenComponent/splash_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/splashscreen.png"),
+              fit: BoxFit.cover)),
+    );
+  }
+}
