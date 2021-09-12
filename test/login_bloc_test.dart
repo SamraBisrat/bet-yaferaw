a49edc7afa0507702b37b41c9bb35f507bf90654
zewetir <zetimebrat@gmail.com>
2021-09-12 17:34:11 +0300

    test added for login

--- test/login_bloc_test.dart
+++ test/login_bloc_test.dart
@@ -1,14 +1,33 @@
+import 'package:bet_yaferaw/Components/LoginComponent/bloc/login_event.dart';
+import 'package:bet_yaferaw/Components/LoginComponent/bloc/login_state.dart';
+import 'package:bet_yaferaw/Repositories/login_repo.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:bet_yaferaw/Components/LoginComponent/bloc/login_bloc.dart';
 
 void main() {
-  // LoginBloc  loginBloc;
   group("login bloc", () {
-    blocTest(
-      "emits [isLoading, LoginButtonPressed] when login successful",
-      // build: () => LoginBloc());
-      // act: loginBloc.add(LoginEvent.LoginButtonPressed),
-      // expect: ()=>[isLoading, LoginButtonPressed]'
-    );
+    UserRepository userRepository;
+    userRepository = UserRepository();
+    LoginBloc loginBloc;
+    loginBloc = LoginBloc(userRepository: userRepository);
+    blocTest("emits[LoginButtonPressed]  when login successful",
+        build: () => LoginBloc(userRepository: userRepository),
+        act: (loginBloc) =>
+            loginBloc.add(LoginButtonPressed("mek@gmail.com", "mek123")),
+        expect: [
+          LoginState(
+              email: "mek@gmail.com",
+              password: 'mek123',
+              isLoading: true,
+              exceptionError: null,
+              tokenSaved: null),
+          LoginState(
+              email: "mek@gmail.com",
+              password: 'mek123',
+              isLoading: false,
+              exceptionError: null,
+              tokenSaved: true),
+        ]);
   });
 }
